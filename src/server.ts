8c26680beb316b899966ffa17303c0c6a34db03b
Ivanst84 <77261169+Ivanst84@users.noreply.github.com>
2024-05-03 22:06:19 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,48 @@
-import express from 'express';
-import colors from 'colors';
-import cors,{CorsOptions} from 'cors';
-import morgan from'morgan'
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec,{swaggerUiOptions} from './config/swagger';
-import router from './router';
-import db from './config/db';
+import express from 'express' 
+import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpec, { swaggerUiOptions } from './config/swagger'
+import router  from './router'
+import db from './config/db'
 
+// Conectar a base de datos
 export async function connectDB() {
     try {
-        await db.authenticate();
-        await db.sync();
-      //  console.log(colors.bgGreen.bold('Conectado a la base de datos'));
+        await db.authenticate()
+        db.sync()
+        // console.log( colors.blue( 'Conexión exitosa a la BD'))
     } catch (error) {
-        console.error(colors.bgRed.white('Error al conectar a la base de datos'));
-        //console.error(error);
-       // throw error; // Propaga el error para manejarlo en otro lugar si es necesario
+        console.log(error)
+        console.log( colors.red.bold( 'Hubo un error al conectar a la BD') )
     }
 }
+connectDB()
 
-connectDB();
-
-const server = express();
+// Instancia de express
+const server = express()
 
 // Permitir conexiones
-console.log("Valor de process.env.FRONTEND_URL:", process.env.FRONTEND_URL);
-const corsOptions: CorsOptions = {
-  origin: function(origin, callback) {
-    console.log('Valor de origin:', origin); // Imprime el valor de origin
-    console.log('Valor de process.env.FRONTEND_URL:', process.env.FRONTEND_URL); // Imprime el valor de process.env.FRONTEND_URL
-    
-    if (!origin || origin === process.env.FRONTEND_URL) {
-      callback(null, true);
-    } else {
-      console.error('Error de CORS:', new Error('No permitido por CORS, verifica la configuración CORS'));
-      callback(new Error('No permitido por CORS, verifica la configuración CORS'));
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS')) 
+        }
     }
-  }
 }
+server.use(cors(corsOptions))
 
-server.use(cors(corsOptions));
+// Leer datos de formularios
+server.use(express.json())
 
+server.use(morgan('dev'))
 
+server.use('/api/products', router)
 
-server.use(express.json());
-server.use(morgan('dev'));
+// Docs
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions) )
 
-server.use('/api/products', router);
-
-// Inicia el servidor solo después de que la conexión a la base de datos sea exitosa
-
-// documentacion
-
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec,swaggerUiOptions));
-
-export default server;
+export default server
