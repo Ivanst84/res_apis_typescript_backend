03c4b148b4f9456781dfbf1ef4fdc5fe02b08bbf
Ivanst84 <77261169+Ivanst84@users.noreply.github.com>
2024-05-03 18:54:20 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,7 @@ const server = express();
 const corsOptions : CorsOptions = {
 
   origin:function(origin,callback){
+	  console.log(origin)
  if(origin ===process.env.FRONTEND_URL ){
 
 callback(null,true)}
