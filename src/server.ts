5e47b12b48d3af2e4497d038dc6016fae847febc
Ivanst84 <77261169+Ivanst84@users.noreply.github.com>
2024-05-04 11:38:27 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,15 +24,17 @@ connectDB()
 const server = express()
 
 // Permitir conexiones
-const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    console.log(origin);
-    // Permitir todas las solicitudes
-    callback(null, true);
-  }
-};
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS')) 
+        }
+    }
+}
+server.use(cors(corsOptions))
 
-server.use(cors(corsOptions));
 
 
 // Leer datos de formularios
