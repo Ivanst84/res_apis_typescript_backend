be7d84de574c0c083b9e4821d756263d7d422e38
Ivanst84 <77261169+Ivanst84@users.noreply.github.com>
2024-05-05 16:37:08 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,48 @@
-import express from 'express';
-import colors from 'colors';
-import cors from 'cors'; // Importa solo 'cors' en lugar de '{ CorsOptions }'
+import express from 'express' 
+import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpec, { swaggerUiOptions } from './config/swagger'
+import router  from './router'
+import db from './config/db'
 
-import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec, { swaggerUiOptions } from './config/swagger';
-import router  from './router';
-import db from './config/db';
-
-// Conectar a la base de datos
+// Conectar a base de datos
 export async function connectDB() {
     try {
-        await db.authenticate();
-        db.sync();
+        await db.authenticate()
+        db.sync()
         // console.log( colors.blue( 'Conexión exitosa a la BD'))
     } catch (error) {
-        console.log(error);
-        console.log(colors.red.bold('Hubo un error al conectar a la BD'));
+        console.log(error)
+        console.log( colors.red.bold( 'Hubo un error al conectar a la BD') )
     }
 }
-connectDB();
+connectDB()
 
 // Instancia de express
-const server = express();
+const server = express()
 
-// Permitir conexiones CORS
-server.use(cors());
+// Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS')) 
+        }
+    }
+}
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
-server.use(express.json());
+server.use(express.json())
 
-server.use(morgan('dev'));
+server.use(morgan('dev'))
 
-server.use('/api/products', router);
+server.use('/api/products', router)
 
 // Docs
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions));
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions) )
 
-export default server;
+export default server
